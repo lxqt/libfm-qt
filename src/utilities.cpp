b6da272eaaf9e1606ad757e4e5dbe2f02023e064
Luís Pereira <luis.artur.pereira@gmail.com>
2016-12-16 15:01:04 +0000

    Fixes QString to char conversion dangling pointers
    Explanation at https://github.com/lxde/pcmanfm-qt/issues/427.
    Closes https://github.com/lxde/pcmanfm-qt/issues/427.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -136,7 +136,8 @@ void renameFile(FmFileInfo *file, QWidget *parent) {
 
   GFile* gf = fm_path_to_gfile(path);
   GFile* parent_gf = g_file_get_parent(gf);
-  GFile* dest = g_file_get_child(G_FILE(parent_gf), new_name.toLocal8Bit().data());
+  QByteArray nm = new_name.toLocal8Bit();
+  GFile* dest = g_file_get_child(G_FILE(parent_gf), nm.constData());
   g_object_unref(parent_gf);
 
   GError* err = NULL;
@@ -193,7 +194,8 @@ _retry:
     return;
 
   GFile* parent_gf = fm_path_to_gfile(parentDir);
-  GFile* dest_gf = g_file_get_child(G_FILE(parent_gf), new_name.toLocal8Bit().data());
+  QByteArray nm = new_name.toLocal8Bit();
+  GFile* dest_gf = g_file_get_child(G_FILE(parent_gf), nm.constData());
   g_object_unref(parent_gf);
 
   GError* err = NULL;
