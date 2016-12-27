10fd56418f978744e903be61bf0b468031317d1f
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-12-27 23:32:51 +0800

    Implement Fm2::UserInfo, which is a cache for uid/gid name mapping.

--- src/core/userinfo.cpp
+++ src/core/userinfo.cpp
@@ -1,9 +1,42 @@
 #include "userinfo.h"
+#include <pwd.h>
+#include <grp.h>
 
 namespace Fm2 {
 
-UserInfo::UserInfo() : QObject() {
+UserInfo* UserInfo::globalInstance_ = nullptr;
 
+UserInfo::UserInfo() : QObject() {
 }
+
+const std::shared_ptr<const User>& UserInfo::userFromId(uid_t uid) {
+    auto it = users_.find(uid);
+    if(it != users_.end())
+        return it->second;
+    std::shared_ptr<const User> user;
+    auto pw = getpwuid(uid);
+    if(pw)
+        user = std::make_shared<User>(uid, pw->pw_name, pw->pw_gecos);
+    return (users_[uid] = user);
+}
+
+const std::shared_ptr<const Group>& UserInfo::groupFromId(gid_t gid) {
+    auto it = groups_.find(gid);
+    if(it != groups_.end())
+        return it->second;
+    std::shared_ptr<const Group> group;
+    auto gr = getgrgid(gid);
+    if(gr)
+        group = std::make_shared<Group>(gid, gr->gr_name);
+    return (groups_[gid] = group);
+}
+
+// static
+UserInfo* UserInfo::instance() {
+   if(!globalInstance_)
+       globalInstance_ = new UserInfo();
+   return globalInstance_;
+}
+
 
 } // namespace Fm2
--- src/core/userinfo.h
+++ src/core/userinfo.h
@@ -2,17 +2,74 @@
 #define FM2_USERINFO_H
 
 #include <QObject>
+#include <string>
+#include <unordered_map>
+#include <sys/types.h>
+#include <memory>
 
 namespace Fm2 {
+
+class User {
+public:
+    User(uid_t uid, const char* name, const char* realName):
+        uid_{uid}, name_{name}, realName_{realName} {
+    }
+
+    uid_t uid() const {
+        return uid_;
+    }
+
+    const std::string& name() const {
+        return name_;
+    }
+
+    const QString& realName() const {
+        return realName_;
+    }
+
+private:
+    uid_t uid_;
+    std::string name_;
+    QString realName_;
+
+};
+
+class Group {
+public:
+    Group(gid_t gid, const char* name): gid_{gid}, name_{name} {
+    }
+
+    gid_t gid() const {
+        return gid_;
+    }
+
+    const std::string& name() const {
+        return name_;
+    }
+
+private:
+    gid_t gid_;
+    std::string name_;
+};
 
 class UserInfo : public QObject {
     Q_OBJECT
 public:
     explicit UserInfo();
 
-Q_SIGNALS:
+    const std::shared_ptr<const User>& userFromId(uid_t uid);
 
-public Q_SLOTS:
+    const std::shared_ptr<const Group>& groupFromId(gid_t gid);
+
+    static UserInfo* instance();
+
+Q_SIGNALS:
+    void changed();
+
+private:
+    std::unordered_map<uid_t, std::shared_ptr<const User>> users_;
+    std::unordered_map<gid_t, std::shared_ptr<const Group>> groups_;
+    static UserInfo* globalInstance_;
 };
 
 } // namespace Fm2
