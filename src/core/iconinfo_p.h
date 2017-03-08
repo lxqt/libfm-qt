5991637ba1ac3e993601bf8f562bf2c3383fee55
Palo Kisa <palo.kisa@gmail.com>
2017-03-08 14:04:14 +0100

    IconEngine: Use weak_ptr for the parent IconInfo
    The IconEngine object can't hold "strong" shared_ptr to it's parent
    (via the QIcon) IconInfo object, because with this we create a circular
    reference and such shared_ptr<IconInfo> object gets never deleted.
    
    shared_ptr<IconInfo> -> QIcon -> IconEngine -> shared_ptr<IconInfo>

--- src/core/iconinfo_p.h
+++ src/core/iconinfo_p.h
@@ -53,7 +53,7 @@ public:
     virtual void virtual_hook(int id, void* data) override;
 
 private:
-    std::shared_ptr<const Fm::IconInfo> info_;
+    std::weak_ptr<const Fm::IconInfo> info_;
 };
 
 IconEngine::IconEngine(std::shared_ptr<const IconInfo> info): info_{info} {
@@ -63,11 +63,12 @@ IconEngine::~IconEngine() {
 }
 
 QSize IconEngine::actualSize(const QSize& size, QIcon::Mode mode, QIcon::State state) {
-    return info_->internalQicon().actualSize(size, mode, state);
+    auto info = info_.lock();
+    return info ? info->internalQicon().actualSize(size, mode, state) : QSize{};
 }
 
 QIconEngine* IconEngine::clone() const {
-    IconEngine* engine = new IconEngine(info_);
+    IconEngine* engine = new IconEngine(info_.lock());
     return engine;
 }
 
@@ -76,28 +77,32 @@ QString IconEngine::key() const {
 }
 
 void IconEngine::paint(QPainter* painter, const QRect& rect, QIcon::Mode mode, QIcon::State state) {
-    return info_->internalQicon().paint(painter, rect, Qt::AlignCenter, mode, state);
+    auto info = info_.lock();
+    if (info)
+        info->internalQicon().paint(painter, rect, Qt::AlignCenter, mode, state);
 }
 
 QPixmap IconEngine::pixmap(const QSize& size, QIcon::Mode mode, QIcon::State state) {
-    return info_->internalQicon().pixmap(size, mode, state);
+    auto info = info_.lock();
+    return info ? info->internalQicon().pixmap(size, mode, state) : QPixmap{};
 }
 
 void IconEngine::virtual_hook(int id, void* data) {
+    auto info = info_.lock();
     switch(id) {
     case QIconEngine::AvailableSizesHook: {
         auto* args = reinterpret_cast<QIconEngine::AvailableSizesArgument*>(data);
-        args->sizes = info_->internalQicon().availableSizes(args->mode, args->state);
+        args->sizes = info ? info->internalQicon().availableSizes(args->mode, args->state) : QList<QSize>{};
         break;
     }
     case QIconEngine::IconNameHook: {
         QString* result = reinterpret_cast<QString*>(data);
-        *result = info_->internalQicon().name();
+        *result = info ? info->internalQicon().name() : QString{};
         break;
     }
     case QIconEngine::IsNullHook: {
         bool* result = reinterpret_cast<bool*>(data);
-        *result = info_->internalQicon().isNull();
+        *result = info ? info->internalQicon().isNull() : true;
         break;
     }
     }
