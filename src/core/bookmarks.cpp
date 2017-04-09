7cf889371a3ecace6416a9d5f8fd809be8252578
Palo Kisa <palo.kisa@gmail.com>
2017-04-10 00:10:32 +0200

    Bookmarks: Check validity of insert position
    Before the C++11 port the underlying container was Glib list and the
    g_list_insert interprets position parameter as "If this is negative, or
    is larger than the number of elements in the list, the new element is
    added on to the end of the list.".
    
    => this commit revamps backward compatibility with pre C++11 libfm-qt.

--- src/core/bookmarks.cpp
+++ src/core/bookmarks.cpp
@@ -41,7 +41,8 @@ Bookmarks::~Bookmarks() {
 }
 
 const std::shared_ptr<const BookmarkItem>& Bookmarks::insert(const FilePath& path, const QString& name, int pos) {
-    auto it = items_.insert(items_.begin() + pos, std::make_shared<const BookmarkItem>(path, name));
+    const auto insert_pos = (pos < 0 || pos > items_.size()) ? items_.cend() : items_.cbegin() + pos;
+    auto it = items_.insert(insert_pos, std::make_shared<const BookmarkItem>(path, name));
     queueSave();
     return *it;
 }
