9e06bd62b3f5f96c23502e35cc61271a25fba98d
Dimitrios Glentadakis <dglent@free.fr>
2018-06-23 03:08:32 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (265 of 265 strings)
    
    Translation: LXQt/LibFM-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/libfm-qt/el/

--- src/translations/libfm-qt_el.ts
+++ src/translations/libfm-qt_el.ts
@@ -1116,7 +1116,7 @@ Modified: %2</source>
     <message>
         <location filename="../core/basicfilelauncher.cpp" line="293"/>
         <source>No default application is set to launch &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Καμία εξ ορισμού εφαρμογή για την εκτέλεση του &apos;%1&apos;</translation>
     </message>
     <message>
         <location filename="../core/basicfilelauncher.cpp" line="334"/>
