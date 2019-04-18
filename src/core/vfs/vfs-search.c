3d23ec7f0857cbc1ae8e42bdd27ab179a7e8344f
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2019-04-19 01:19:08 +0800

    Fix the license header of code we took from libfm (they should use LGPL2 instead of GPL2). - src/core/vfs/vfs-menu.c - src/core/vfs/vfs-search.c These files are taken from libfm which has been relicensed to LGPL 2 on 2014-09-05 by Andrey N. Gritsenko after consulting other contributors.

--- src/core/vfs/vfs-search.c
+++ src/core/vfs/vfs-search.c
@@ -1,25 +1,22 @@
 /*
- * fm-vfs-search.c
+ *      fm-vfs-search.c
  * 
- * Copyright 2012 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
- * Copyright 2010 Shae Smittle <starfall87@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
+ *      Copyright 2012 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *      Copyright 2010 Shae Smittle <starfall87@gmail.com>
+ *
+ *      This library is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU Lesser General Public
+ *      License as published by the Free Software Foundation; either
+ *      version 2.1 of the License, or (at your option) any later version.
+ *
+ *      This library is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      Lesser General Public License for more details.
+ *
+ *      You should have received a copy of the GNU Lesser General Public
+ *      License along with this library; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifdef HAVE_CONFIG_H
