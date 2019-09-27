f1dea66d31aa6ec189a44e26c6dcf1316fe2f349
Moo <hazap@hotmail.com>
2019-09-27 09:49:01 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (290 of 290 strings)
    
    Translation: LXQt/LibFM-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/libfm-qt/lt/

--- src/translations/libfm-qt_lt.ts
+++ src/translations/libfm-qt_lt.ts
@@ -833,10 +833,10 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message numerus="yes">
         <location filename="../filepropsdialog.cpp" line="386"/>
         <source>%Ln files</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%Ln failas</numerusform>
+            <numerusform>%Ln failai</numerusform>
+            <numerusform>%Ln failų</numerusform>
         </translation>
     </message>
     <message>
@@ -883,7 +883,7 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../core/filetransferjob.cpp" line="465"/>
         <source>Cannot copy a directory into itself!</source>
-        <translation type="unfinished"></translation>
+        <translation>Negalima kopijuoti katalogą į jį patį!</translation>
     </message>
     <message>
         <location filename="../core/filetransferjob.cpp" line="516"/>
@@ -946,7 +946,7 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../foldermenu.cpp" line="166"/>
         <source>By Deletion Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Pagal ištrynimo laiką</translation>
     </message>
     <message>
         <location filename="../foldermenu.cpp" line="169"/>
@@ -966,7 +966,7 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../foldermenu.cpp" line="172"/>
         <source>By File Group</source>
-        <translation type="unfinished"></translation>
+        <translation>Pagal failo grupę</translation>
     </message>
     <message>
         <location filename="../foldermenu.cpp" line="178"/>
@@ -994,37 +994,37 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../foldermodel.cpp" line="274"/>
         <source>Location:</source>
-        <translation type="unfinished">Vieta:</translation>
+        <translation>Vieta:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="277"/>
         <source>File type:</source>
-        <translation type="unfinished">Failo tipas:</translation>
+        <translation>Failo tipas:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="281"/>
         <source>File size:</source>
-        <translation type="unfinished">Failo dydis:</translation>
+        <translation>Failo dydis:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="285"/>
         <source>Last modified:</source>
-        <translation type="unfinished">Paskutinis pakeitimas:</translation>
+        <translation>Paskutinis pakeitimas:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="286"/>
         <source>Last accessed:</source>
-        <translation type="unfinished">Paskutinė prieiga:</translation>
+        <translation>Paskutinė prieiga:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="290"/>
         <source>Owner:</source>
-        <translation type="unfinished">Savininkas:</translation>
+        <translation>Savininkas:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="291"/>
         <source>Group:</source>
-        <translation type="unfinished">Grupė:</translation>
+        <translation>Grupė:</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="359"/>
@@ -1049,7 +1049,7 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../foldermodel.cpp" line="371"/>
         <source>Deleted</source>
-        <translation type="unfinished"></translation>
+        <translation>Ištrintas</translation>
     </message>
     <message>
         <location filename="../foldermodel.cpp" line="374"/>
@@ -1067,12 +1067,12 @@ Ar norite vietoj to, juos ištrinti?</translation>
     <message>
         <location filename="../folderview.cpp" line="373"/>
         <source>Auto-resize columns</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatiškai keisti stulpelių dydį</translation>
     </message>
     <message>
         <location filename="../folderview.cpp" line="390"/>
         <source>Visible Columns</source>
-        <translation type="unfinished"></translation>
+        <translation>Matomi stulpeliai</translation>
     </message>
 </context>
 <context>
@@ -1611,7 +1611,7 @@ Pakeista: %2</translation>
     <message>
         <location filename="../filesearch.ui" line="122"/>
         <source>Search in subdirectories</source>
-        <translation type="unfinished"></translation>
+        <translation>Ieškoti pakatalogiuose</translation>
     </message>
     <message>
         <location filename="../filesearch.ui" line="370"/>
