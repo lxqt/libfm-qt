b9ce9ab0d0fdc190069b595817184be83077eacd
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-10-26 20:22:44 +0800

    weblate commits (#738)
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Co-authored-by: Masamichi Ito <ito32bit+github@gmail.com>
    Co-authored-by: pavol polonec <pavolpolonec@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/ja/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/sk/
    Translation: LXQt Desktop/libfm-qt

--- src/translations/libfm-qt_sk.ts
+++ src/translations/libfm-qt_sk.ts
@@ -243,17 +243,17 @@
     <message>
         <location filename="../file-props.ui" line="330"/>
         <source>Emblem:</source>
-        <translation type="unfinished"></translation>
+        <translation>Emblém:</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="342"/>
         <source>Choose emblem</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyberte emblém</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="361"/>
         <source>Clear emblem</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstrániť emblém</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="414"/>
@@ -437,7 +437,7 @@
     <message>
         <location filename="../dndactionmenu.cpp" line="42"/>
         <source>Cannot drop here</source>
-        <translation type="unfinished"></translation>
+        <translation>Sem nie je monžné položku presunúť</translation>
     </message>
 </context>
 <context>
@@ -1011,7 +1011,7 @@ Prajete si ich vymazať natrvalo?</translation>
     <message>
         <location filename="../foldermenu.cpp" line="51"/>
         <source>Empty Trash</source>
-        <translation type="unfinished">Vyprázdniť kôš</translation>
+        <translation>Vyprázdniť kôš</translation>
     </message>
     <message>
         <location filename="../foldermenu.cpp" line="63"/>
@@ -1395,7 +1395,7 @@ Zmenené: %2</translation>
     <message>
         <location filename="../renamedialog.cpp" line="108"/>
         <source>&lt;p&gt;&lt;b&gt;The file cannot overwrite itself.&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Please select another name.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;&lt;b&gt;Súbor nemôže prepísať sám seba.&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Vyberte prosím iný názov.&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
