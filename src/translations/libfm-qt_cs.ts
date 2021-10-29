9822a140744b6caa4205723735d782318769cbfd
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-10-29 18:56:32 +0200

    weblate commits (#744)
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/cs/
    Translation: LXQt Desktop/libfm-qt

--- src/translations/libfm-qt_cs.ts
+++ src/translations/libfm-qt_cs.ts
@@ -243,17 +243,17 @@
     <message>
         <location filename="../file-props.ui" line="330"/>
         <source>Emblem:</source>
-        <translation type="unfinished"></translation>
+        <translation>Emblém:</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="342"/>
         <source>Choose emblem</source>
-        <translation type="unfinished"></translation>
+        <translation>Zvolit emblém</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="361"/>
         <source>Clear emblem</source>
-        <translation type="unfinished"></translation>
+        <translation>Vymazat emblém</translation>
     </message>
     <message>
         <location filename="../file-props.ui" line="414"/>
@@ -437,7 +437,7 @@
     <message>
         <location filename="../dndactionmenu.cpp" line="42"/>
         <source>Cannot drop here</source>
-        <translation type="unfinished"></translation>
+        <translation>Není možné sem přetáhnout</translation>
     </message>
 </context>
 <context>
@@ -1011,7 +1011,7 @@ Chcete je namísto toho přímo odstranit?</translation>
     <message>
         <location filename="../foldermenu.cpp" line="51"/>
         <source>Empty Trash</source>
-        <translation type="unfinished">Vysypat koš</translation>
+        <translation>Vysypat koš</translation>
     </message>
     <message>
         <location filename="../foldermenu.cpp" line="63"/>
@@ -1395,7 +1395,7 @@ Změněno: %2</translation>
     <message>
         <location filename="../renamedialog.cpp" line="108"/>
         <source>&lt;p&gt;&lt;b&gt;The file cannot overwrite itself.&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Please select another name.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;&lt;b&gt;Soubor není možné přepsat sám sebou.&lt;/b&gt;&lt;/p&gt;&lt;p&gt;Zvolte jiný název.&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
