600b308b4a2a9dd4ce6c3fed42af7ddb24ae310f
Weblate <noreply@weblate.org>
2022-03-29 16:54:16 +0200

    weblate commits
    Co-authored-by: Biukang <niubiukang@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/zh_CN/
    Translation: LXQt Desktop/libfm-qt

--- src/translations/libfm-qt_zh_CN.ts
+++ src/translations/libfm-qt_zh_CN.ts
@@ -553,7 +553,7 @@ What do you want to do with it?</source>
     <message>
         <location filename="../filedialog.cpp" line="200"/>
         <source>Show Hidden</source>
-        <translation type="unfinished">显示隐藏文件</translation>
+        <translation>显示隐藏文件</translation>
     </message>
     <message>
         <location filename="../filedialog.cpp" line="212"/>
@@ -568,7 +568,7 @@ What do you want to do with it?</source>
     <message>
         <location filename="../filedialog.cpp" line="224"/>
         <source>Smooth Scrolling</source>
-        <translation type="unfinished"></translation>
+        <translation>平滑滚动</translation>
     </message>
     <message>
         <location filename="../filedialog.cpp" line="241"/>
