4e9ac17778d180089b7b7d3b3b6d5fb10e3adb0c
Weblate <noreply@weblate.org>
2022-11-04 18:41:42 +0100

    weblate commits
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: NightFeather <ss900328@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: raiden <dakal@ml1.net>
    Co-authored-by: wallon <franckjl@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/libfm-qt/zh_TW/
    Translation: LXQt Desktop/libfm-qt

--- src/translations/libfm-qt_nb_NO.ts
+++ src/translations/libfm-qt_nb_NO.ts
@@ -124,7 +124,7 @@
     <message>
         <location filename="../exec-file.ui" line="99"/>
         <source>Remember for next files of this kind</source>
-        <translation type="unfinished"></translation>
+        <translation>Husk for kommende filer av denne typen</translation>
     </message>
 </context>
 <context>
