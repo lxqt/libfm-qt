lxqt/libfm-qt

commit dbfd59182ab5fdaee42f091cf7ca2d0a5f23c96e

Assign color and font dialogs to the parent window (#1074)
This ensures that the dialogs are attached to the main window.

src/colorbutton.cpp
@@ -32,7 +32,7 @@ ColorButton::~ColorButton() {
 }
 
 void ColorButton::onClicked() {
-    QColorDialog dlg(color_);
+    QColorDialog dlg(color_, this);
     if(dlg.exec() == QDialog::Accepted) {
         setColor(dlg.selectedColor());
     }

src/fontbutton.cpp
@@ -32,7 +32,7 @@ FontButton::~FontButton() {
 }
 
 void FontButton::onClicked() {
-    QFontDialog dlg(font_);
+    QFontDialog dlg(font_, this);
     if(dlg.exec() == QDialog::Accepted) {
         setFont(dlg.selectedFont());
     }

commit 3d5d5f205a67aff21333858b3c056510943e96b4

Fixed a regression in color and font dialogs
The regression happened in https://github.com/lxqt/libfm-qt/commit/dbfd59182ab5fdaee42f091cf7ca2d0a5f23c96e.

*Please never tell me again that being against obsessions is a bad attitude of mine* ;) They cause headaches!

Fixes https://github.com/lxqt/pcmanfm-qt/issues/2170

src/colorbutton.cpp
@@ -32,7 +32,7 @@ ColorButton::~ColorButton() {
 }
 
 void ColorButton::onClicked() {
-    QColorDialog dlg(color_, this);
+    QColorDialog dlg(color_, window());
     if(dlg.exec() == QDialog::Accepted) {
         setColor(dlg.selectedColor());
     }

src/fontbutton.cpp
@@ -32,7 +32,7 @@ FontButton::~FontButton() {
 }
 
 void FontButton::onClicked() {
-    QFontDialog dlg(font_, this);
+    QFontDialog dlg(font_, window());
     if(dlg.exec() == QDialog::Accepted) {
         setFont(dlg.selectedFont());
     }