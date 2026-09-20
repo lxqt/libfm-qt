85783148a761bf0e2f51e3b893896f2e6f59e52d
stefonarch <standreas@riseup.net>
2026-09-20 08:25:00 +0200

    Removed version check for Qt < 6.8

--- src/core/iconinfo_p.h
+++ src/core/iconinfo_p.h
@@ -106,13 +106,7 @@ bool IconEngine::isNull() {
 QPixmap IconEngine::scaledPixmap(const QSize &size, QIcon::Mode mode, QIcon::State state, qreal scale) {
     auto info = info_.lock();
     return info ?
-           // According to Qt doc, "size" is device-independent since Qt 6.8,
-           // while it was device-dependent prior to Qt 6.8.
-#if (QT_VERSION < QT_VERSION_CHECK(6,8,0))
-           info->internalQicon().pixmap((size.toSizeF() / scale).toSize(), scale, mode, state)
-#else
            info->internalQicon().pixmap(size, scale, mode, state)
-#endif
            : QPixmap{};
 }
 
